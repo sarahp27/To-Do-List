aeb8a1b70afa45268207e4ff625d86dd7b85638f
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-17 14:58:30 +0500

    new changes

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
+
 
 const ToDoList = () => {
     const toggle = () => setModal(!modal)
+    const [modal, setModal] = useState(false);
+    
 
     return (
         <>
@@ -9,7 +13,14 @@ const ToDoList = () => {
             <h1> ToDo List</h1>
             <button onClick={toggle}>Add task</button>
         </div>
+        <div className = "task-container">
+            <form>
+                <input type ="text" placeholder='title'/>
+                <input type ="text" placeholder='description'/>
+                <input type ="date" placeholder='duedate'/>
 
+            </form>
+        </div>
         </>
     );
 }
