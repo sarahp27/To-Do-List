dea337d4ddab947575f0116eb7f2f52959047d27
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-17 18:53:11 -0700

    Add Task in Card

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import CreateTask from '../Modals/CreateTask';
+import Card from './Card';
 
 const ToDoList = () => {
     const toggle = () => setModal(!modal)
     const [modal, setModal] = useState(false);
     const [taskList, setTaskList] = useState([]);
 
+    useEffect(() => {
+        let array = localStorage.getItem("taskList")
+        
+        if (array){
+            let obj = JSON.parse(array)
+            setTaskList(obj)
+        }
+    },[])
+
     const saveTask = (taskObj) =>{
         let tempList = taskList
         tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON. stringify(tempList))
         setTaskList(tempList) 
         setModal(false)
 
@@ -22,7 +33,7 @@ const ToDoList = () => {
             <button className='btn btn-primary mt-2' onClick = {() => setModal(true)}>Add task</button>
         </div>
         <div className = "task-container">
-            {taskList.map((obj) => <li>{obj.Name}</li>)}
+            {taskList.map((obj) => <Card taskObj/>)}
         </div>
         <CreateTask toggle={toggle} modal={modal} save= {saveTask}/>
         </>
